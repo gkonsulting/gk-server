6b185a7d819d90cf4b43591519a8df40394c8675
ianevangelista <ianevangelista1999@gmail.com>
2020-11-12 11:05:04 +0100

    update loads

--- client/src/pages/Movie/Update/[id].tsx
+++ client/src/pages/Movie/Update/[id].tsx
@@ -17,70 +17,78 @@ export const updateMovie: React.FC<{}> = ({}) => {
     const [updateMovie] = useUpdateMovieMutation();
     const router = useRouter();
 
-    return (
-        <>
-            <Navbar />
-            <Wrapper variant="small">
-                <Formik
-                    initialValues={{
-                        title: movie?.title as string,
-                        description: movie?.description as string,
-                        poster: movie?.poster as string,
-                        reason: movie?.reason as string,
-                        rating: movie?.rating as string,
-                    }}
-                    onSubmit={async (values) => {
-                        await updateMovie({
-                            variables: {
-                                id: movie?.id,
-                                input: values,
-                            },
-                        });
-                        router.back();
-                    }}
-                >
-                    {({ isSubmitting }) => (
-                        <Form>
-                            <InputField
-                                name="title"
-                                placeholder="Title"
-                                label="Title"
-                            />
-                            <InputField
-                                name="description"
-                                placeholder="Description"
-                                label="Description"
-                            />
-                            <InputField
-                                name="rating"
-                                placeholder="IMDB-rating"
-                                label="Rating"
-                            />
-                            <InputField
-                                name="poster"
-                                placeholder="IMDB-poster link"
-                                label="IMDB-poster link"
-                            />
-                            <InputField
-                                name="reason"
-                                placeholder="Why do you want to watch this movie?"
-                                label="Reason why?"
-                            />
-                            <Flex mt={4}>
-                                <Button
-                                    isLoading={isSubmitting}
-                                    type="submit"
-                                    variantColor="teal"
-                                >
-                                    Update movie
-                                </Button>
-                            </Flex>
-                        </Form>
-                    )}
-                </Formik>
-            </Wrapper>
-        </>
-    );
+    if (loading) {
+        return <div>loading....</div>;
+    }
+
+    if (!loading && !data?.getMovie) {
+        return <div>No movie found</div>;
+    } else {
+        return (
+            <>
+                <Navbar />
+                <Wrapper variant="small">
+                    <Formik
+                        initialValues={{
+                            title: movie?.title as string,
+                            description: movie?.description as string,
+                            poster: movie?.poster as string,
+                            reason: movie?.reason as string,
+                            rating: movie?.rating as string,
+                        }}
+                        onSubmit={async (values) => {
+                            await updateMovie({
+                                variables: {
+                                    id: movie?.id,
+                                    input: values,
+                                },
+                            });
+                            router.back();
+                        }}
+                    >
+                        {({ isSubmitting }) => (
+                            <Form>
+                                <InputField
+                                    name="title"
+                                    placeholder="Title"
+                                    label="Title"
+                                />
+                                <InputField
+                                    name="description"
+                                    placeholder="Description"
+                                    label="Description"
+                                />
+                                <InputField
+                                    name="rating"
+                                    placeholder="IMDB-rating"
+                                    label="Rating"
+                                />
+                                <InputField
+                                    name="poster"
+                                    placeholder="IMDB-poster link"
+                                    label="IMDB-poster link"
+                                />
+                                <InputField
+                                    name="reason"
+                                    placeholder="Why do you want to watch this movie?"
+                                    label="Reason why?"
+                                />
+                                <Flex mt={4}>
+                                    <Button
+                                        isLoading={isSubmitting}
+                                        type="submit"
+                                        variantColor="teal"
+                                    >
+                                        Update movie
+                                    </Button>
+                                </Flex>
+                            </Form>
+                        )}
+                    </Formik>
+                </Wrapper>
+            </>
+        );
+    }
 };
 
 export default withApollo({ ssr: true })(updateMovie);
