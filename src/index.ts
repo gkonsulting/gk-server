ed6aeb99532c1c287c0d8cdbe74b921e66538eb7
ianevangelista <ianevangelista1999@gmail.com>
2020-11-14 11:39:30 +0100

    lax

--- src/index.ts
+++ src/index.ts
@@ -71,7 +71,7 @@ const main = async () => {
             cookie: {
                 // domain: "guttakrutt-backend.herokuapp.com",
                 maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 år
-                sameSite: __prod__ ? "none" : "lax", // csrf
+                sameSite: "lax", // csrf
                 secure: __prod__, // hvis true funker det bare i https
             },
             saveUninitialized: false,
