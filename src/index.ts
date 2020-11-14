3473b2ef1b67388da70d6aa8342066b272ee17ec
ianevangelista <ianevangelista1999@gmail.com>
2020-11-14 12:08:30 +0100

    http only test

--- src/index.ts
+++ src/index.ts
@@ -69,9 +69,8 @@ const main = async () => {
             secret: process.env.SECRET, //krypterer userid key, express session setter cookie, req sender cookie, server dekrypterer, req til redis og får value userid
             resave: false,
             cookie: {
-                domain: __prod__ ? "herokuapp.com" : "",
                 maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 år
-                sameSite: "lax", // csrf
+                sameSite: __prod__ ? "none" : "lax", // csrf
                 secure: __prod__, // hvis true funker det bare i https
                 httpOnly: true,
             },
