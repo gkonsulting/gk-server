a88da3ecb60167e88479df0457de859c35938cb5
ianevangelista <ianevangelista1999@gmail.com>
2020-11-14 12:21:45 +0100

    rm domain

--- src/index.ts
+++ src/index.ts
@@ -69,8 +69,7 @@ const main = async () => {
             secret: process.env.SECRET, //krypterer userid key, express session setter cookie, req sender cookie, server dekrypterer, req til redis og får value userid
             resave: false,
             cookie: {
-                path: '/',
-                domain: __prod__ ? ".herokuapp.com" : "",
+                path: "/",
                 maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 år
                 sameSite: __prod__ ? "none" : "lax", // csrf
                 secure: __prod__, // hvis true funker det bare i https
